25602cb1459c380b0c57aad6087e03e5df6d1411
TeladSiuol <l.dalet4@gmail.com>
2023-10-03 11:13:25 +0400

    TP1.2  done

--- M.Palermo/LangageC/TP1/TrameTP1/src/TP1_2.c
+++ M.Palermo/LangageC/TP1/TrameTP1/src/TP1_2.c
@@ -4,5 +4,16 @@
 void TP1_2()
 {
     printf("TP1_2\n");
-    
+
+    int j, entier, R1 = 0, R2=1;
+
+    printf("entrer un entier :\n");
+    scanf("%d", &entier);
+
+    for (j=1; j<=entier; j++){
+        R1 = R1 + j;
+        R2 = R2 * j;
+    }
+    printf("%d\n", R1);
+    printf("%d\n", R2);
 }
